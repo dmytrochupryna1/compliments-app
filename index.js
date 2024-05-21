db5695dd45705c48694c86a2a93d3523d071e613
Dimitri Chupryna <dmytro.chupryna.1@gmail.com>
2024-05-21 18:37:17 +0100

    firebase connected + event listeners
    * event listener that adds message to db on click of "publish" button
    * event listener that switches "Asya" to "Dima" and vice versa on click of"Author"  field

--- index.js
+++ index.js
@@ -1,2 +1,64 @@
-let textEl = document.getElementById('text-el');
-textEl.textContent = 'This is a test';
+// an app that lets an author add a message into the input field, then click the "Publish" button, and 
+// message is added to the unordered list, and is rendered on the page
+// app connects to firebase db
+// an input field takes text
+// an "Author" field default says "Dima" but onclick changes to "Asya
+// once the message is clicked on in the list, a "[x]" message appears with the number of "likes"
+// messages are order to the list in reverse order, meaning the latest one is on top
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const authorEl = document.getElementById('author-el');
+const inputEl = document.getElementById('input-el');
+const publishBtn = document.getElementById('publish-btn');
+;
+
+// connect firebase db here, db url is https://realtime-database-7e33c-default-rtdb.europe-west1.firebasedatabase.app/
+
+const appSettings = {
+    databaseURL: 'https://realtime-database-7e33c-default-rtdb.europe-west1.firebasedatabase.app/'
+}
+
+const app = initializeApp(appSettings);
+const db = getDatabase(app);
+const messagesRef = ref(db, 'messages');
+
+
+
+
+
+
+
+
+
+
+
+// add message to db on click
+
+publishBtn.addEventListener('click', function() {
+    push(messagesRef, inputEl.value)
+    console.log(`${inputEl.value } is added to "messages" db`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+authorEl.addEventListener('click', function() {
+
+    if (authorEl.textContent === 'Dima') {
+        authorEl.textContent = 'Asya';
+    } else {
+        authorEl.textContent = 'Dima';
+    }
+
+    // authorEl.textContent = 'Asya';
+})
